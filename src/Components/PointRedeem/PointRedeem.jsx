0b681e04a04ae0dfbb9da78892dfe5e67d7b4d28
Gift Chika <113692242+GiftChika@users.noreply.github.com>
2023-09-24 17:47:52 -0600

    i added some mobile responsivness

--- src/Components/PointRedeem/PointRedeem.jsx
+++ src/Components/PointRedeem/PointRedeem.jsx
@@ -10,38 +10,38 @@ import icon3 from '../PointRedeem/images/clock.svg'
 const PointRedeem = () => {
     return ( 
         <DashBoardLayout>
-           <div className='flex flex-col' >
+           <div className='flex flex-col ' >
             {/* top part after nav bar */}
-                <div className='flex flex-row  px-4 rounded-md  py-2 bg-gradient-to-r from-[#92ABFE] to-[#FFF741]'>
+                <div className='flex flex-row  px-4 rounded-md justify-between  py-2 bg-gradient-to-r from-[#92ABFE] to-[#FFF741]'>
                     <div className='flex flex-col gap-2 ' >
-                        <div className='text-[8px] font-[600] text-[#000000]'>REDEEM YOUR EARNED POINTS WITH <br /> AREMXYPLUG.</div>
-                        <div className='text-[6.67px] font-[400] text-[#000000]'>Redeem all your earned points to real money, withdrawn to <br />
+                        <div className='text-[8px] font-[600] md:text-[13.75px] md:leading-[20.63px] text-[#000000] leading-[12px]'>REDEEM YOUR EARNED POINTS WITH <br /> AREMXYPLUG.</div>
+                        <div className='text-[6.67px] font-[400] md:text-[11.46px] md:leading-[14.9px] text-[#000000] leading-[8.67px]'>Redeem all your earned points to real money, withdrawn to <br />
                              your bank account instantly without any hassle,free and enjoy!</div>
                     </div>
-                    <div><img className='w-[88.38px] h-[]' src= {cloud} alt="" /> </div>
+                    <div><img className='w-[88.38px] md:w-[151.9px] md:h-[85.9px]' src= {cloud} alt="" /> </div>
                 </div>
 
                 {/* text lines after top part */}
-                <div className='text-[8px] font-[500] mt-[15px] text-[#7C7C7C]' >How much points would you like to redeem to real money?</div>
-                <div className='font-[500] text-[7px] py-1 mt-[20px] text-center bg-primary text-white'>Real-timePoints Redeem Tracker</div>
+                <div className='text-[8px] font-[500] mt-[15px] md:mt-[20px] md:text-[11.46px] md:leading-[14.9px] text-[#7C7C7C]' >How much points would you like to redeem to real money?</div>
+                <div className='font-[500] text-[7px] py-1 mt-[20px] md:mt-[24px] text-center px-4 md:text-[9.17px] md:flex md:flex-row md:w-[200px] md:py-1 md:rounded-sm md:leading-[11.5px] bg-primary text-white'>Real-timePoints Redeem Tracker</div>
                 
                 {/* Section with input boxes */}
-                <div className='mt-[20px] flex flex-row'>
-                    <div className='border-[1px] w-[271.75px] h-[30px]  px-2 py- border-slate-200'><input type="text" className='  text-[7px] font-[600] text-[#7C7C7C]' placeholder='Amount to Redeem' /> </div>
-                    <div className='h-[30px] w-[50px] gap-2 flex flex-row px-3 py-2 bg-primary items-center   '>
-                        <div> <img src= {icon}   alt="" /> </div>
-                        <div> <img src= {arrowdown}  alt="" /> </div>
+                <div className='mt-[20px] md:mt-[30px] flex flex-row '>
+                    <div className='border-[1px] w-[85%] md:w-[92%] h-[30px] md:h-[40px]  px-2 py-0 md:pt-1  border-slate-200'><input type="text" className='  text-[7px] md:text-[9.2px] font-[600]  text-[#7C7C7C]' placeholder='Amount to Redeem' /> </div>
+                    <div className='h-[30px] md:h-[40px] w-[15%] md:w-[8%] gap-2 flex flex-row px-3 py-2 bg-primary items-center   '>
+                        <div> <img src= {icon} className='md:w-[13.75px] md:h-[13.75px]'  alt="" /> </div>
+                        <div> <img src= {arrowdown} className='md:w-[13.75px] md:h-[13.75px]'  alt="" /> </div>
                     </div>
                 </div>
-                <div className='flex flex-col items-center mt-[8px] text-[#7C7C7C] gap-2 font-[500] text-[7px] '>
+                <div className='flex flex-col items-center mt-[8px] text-[#7C7C7C] gap-2 font-[500] text-[7px] md:text-[9.2px] '>
                     <div className='border-[1px] border-slate-200 px-1 py-0 rounded-sm' >Minimum 100 PTS</div>
                     <div className='border-[1px] border-slate-200 pl-1 pr-3 py-0 rounded-sm'>Available Points Balance (50,000.00)</div>
                 </div>
                 <div className='mt-[7px] flex flex-row'>
-                    <div className='border-[1px] w-[271.75px] h-[30px]  px-2 py- border-slate-200'><input type="text" className='  text-[7px] font-[600] text-[#7C7C7C]' placeholder='Amount to Receive' /> </div>
-                    <div className='h-[30px] w-[50px] gap-2 flex flex-row px-3 py-2 bg-primary items-center   '>
-                        <div> <img src= {flag}   alt="" /> </div>
-                        <div> <img src= {arrowdown}  alt="" /> </div>
+                    <div className='border-[1px] w-[85%] md:w-[92%] h-[30px] md:h-[40px]  px-2 py-0 md:pt-1 border-slate-200'><input type="text" className='  text-[7px] md:text-[9.2px] font-[600] text-[#7C7C7C]' placeholder='Amount to Receive' /> </div>
+                    <div className='h-[30px] md:h-[40px] w-[15%] md:w-[8%] gap-2 flex flex-row px-3 py-2 bg-primary items-center   '>
+                        <div> <img src= {flag} className='md:w-[13.75px] md:h-[13.75px]'   alt="" /> </div>
+                        <div> <img src= {arrowdown} className='md:w-[13.75px] md:h-[13.75px]'  alt="" /> </div>
                     </div>
                 </div>
                 {/* Section after input boxes */}
@@ -59,8 +59,11 @@ const PointRedeem = () => {
                         <div>Completion Time - Instantly.</div>
                     </div>
                 </div>
-                <div className='text-[12px] mt-[50px] py-3 rounded-md font-[500] text-center text-white bg-primary'>Proceed</div>
-                <div className='flex flex-row items-center justify-center mt-[40px] gap-2'>
+                <div className='flex flex-col justify-center md:items-center'>
+                    <div className='text-[12px] mt-[50px] md:mt-[40px] md:w-[100px] md:py-1 md:rounded-md md:px-6   py-3 rounded-md font-[500] text-center text-white bg-primary'>Proceed</div>
+                </div>
+                
+                <div className='flex flex-row items-center justify-center mt-[40px] md:mt-[250px] gap-2'>
                     <div className='text-[8px] font-[600] text-black'>You need help?</div>
                     <div className=' bg-primary text-white text-[7px] px-2 py-1 leading-[10.5px] rounded-lg text-center '>Contact us</div>
                 </div>
