b6457f50756eb17725c532eabfc883052a42401b
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-09-26 17:49:48 +0100

    Currency Conversion page and fiat page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
-import { Layout } from "./Components/Layout/Layout";
+// import { Layout } from "./Components/Layout/Layout";
+import CurrencyConversion from "../src/Components/CurrencyConversion/currencyConversion";
+import FiatConversion from "./Components/CurrencyConversion/FiatConversion";
+
 
 function App() {
   return (
     <>
-      <Layout />
+      {/* <Layout /> */}
+    <CurrencyConversion/>
+    {/* <FiatConversion/> */}
     </>
   );
 }
