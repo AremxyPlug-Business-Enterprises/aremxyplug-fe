1d7999ba21a7ba0928e69c8f03bc69a70b169223
Umolo Blessing Johnson <umoloblessing009@gmail.com>
2023-09-26 14:42:15 +0100

    Data-top-up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Layout } from "./Components/Layout/Layout";
-// import DataTopUpPage from "./Components/Dashboard/DashboardComponents/DataTopUpPage/DataTopUpPage";
 // import { TransferPage } from "./Components/Dashboard/DashboardComponents/TransferComponent/TransferPage"
 
 function App() {
