b76d46dc223e866749fbf438796ad4ccf59b41b5
Gift Chika <113692242+GiftChika@users.noreply.github.com>
2023-09-27 14:52:37 -0600

    made corrections to point redeem page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import { Layout } from "./Components/Layout/Layout";
+// import { Layout } from "./Components/Layout/Layout";
 // import { TransferPage } from "./Components/Dashboard/DashboardComponents/TransferComponent/TransferPage"
-
-
+import PointRedeem from "./Components/Dashboard/DashboardComponents/PointRedeem/PointRedeem";
 function App() {
   return (
     <>
-      <Layout />
-      
+      {/* <Layout /> */}
+      <PointRedeem/>
     </>
   );
 }
