7802575c8adb707cd6ae0cf65830304cea3b1df1
dedayorr <dedayo16@gmail.com>
2023-11-04 09:07:10 +0100

    conflict

--- src/Components/Dashboard/DashboardComponents/DataTopUpPage/DataBundles/EtisalatDataBundle/EtisalatDataBundle.jsx
+++ src/Components/Dashboard/DashboardComponents/DataTopUpPage/DataBundles/EtisalatDataBundle/EtisalatDataBundle.jsx
@@ -50,11 +50,10 @@ const EtisalatDataBundle = () => {
   const [paymentAmount, setPaymentAmount] = useState("");
   const [codes, setCodes] = useState(false);
 
-  const handleCodes =()=> {
+  const handleCodes = () => {
     setCodes(false);
     setCodes(true);
-  }
-
+  };
 
   const handleShowPayment = () => {
     setShowPayment(!showPayment);
@@ -300,7 +299,7 @@ const EtisalatDataBundle = () => {
   };
 
   console.log("confirm:", confirm);
-  
+
   return (
     <DashBoardLayout>
       <div
@@ -342,31 +341,42 @@ const EtisalatDataBundle = () => {
           </div>
 
           {/* =========================Select/Add Recipient===================== */}
-          
+
           <div className="flex gap-[10%] mt-[40px] md:w-full md:justify-between md:gap-[10%] ">
             <div className="w-full flex items-center justify-between border text-[10px] md:py-[15px] md:w-[50%] rounded-[5px] h-[25px] p-1 md:text-[14px] lg:h-[45px] lg:text-[16px] lg:rounded-[10px] lg:border-[1px] lg:border-[#0003]">
-            <Link to="/DataBundleSelectRecipient" style={{ display: 'inline-flex', width: '100%',}} className="justify-between">
-              <p className="font-semibold">Select Recipient</p>
-              <img
-                className="w-[13px] h-[13px] lg:w-[29px] lg:h-[29px]"
-                src={Recipient}
-                alt=""
-              />
+              <Link
+                to="/DataBundleSelectRecipient"
+                style={{ display: "inline-flex", width: "100%" }}
+                className="justify-between"
+              >
+                <p className="font-semibold">Select Recipient</p>
+                <img
+                  className="w-[13px] h-[13px] lg:w-[29px] lg:h-[29px]"
+                  src={Recipient}
+                  alt=""
+                />
               </Link>
             </div>
             <div className="w-full flex items-center justify-between border text-[10px] md:py-[15px] md:w-[40%] md:mr-[9%]  rounded-[5px] h-[25px] p-1 md:text-[14px] lg:h-[45px] lg:text-[16px] lg:rounded-[10px] lg:border-[1px] lg:border-[#0003]">
-            <Link to="/DataBundleAddRecipient" style={{ display: 'inline-flex', width: '100%' }} className="justify-between">
-              <p className="font-semibold">Add Recipient</p>
-              <img
-                className="w-[13px] h-[13px] lg:w-[29px] lg:h-[29px]"
-                src={Recipient2}
-                alt=""
-              />
+              <Link
+                to="/DataBundleAddRecipient"
+                style={{ display: "inline-flex", width: "100%" }}
+                className="justify-between"
+              >
+                <p className="font-semibold">Add Recipient</p>
+                <img
+                  className="w-[13px] h-[13px] lg:w-[29px] lg:h-[29px]"
+                  src={Recipient2}
+                  alt=""
+                />
               </Link>
             </div>
           </div>
 
-          <div onClick={handleCodes} className="flex mt-[35px] my-[30px] md:w-[100%] md:gap-[10%]">
+          <div
+            onClick={handleCodes}
+            className="flex mt-[35px] my-[30px] md:w-[100%] md:gap-[10%]"
+          >
             <div className="rounded-[4px] w-full bg-primary text-white md:w-[50%] h-[30px] lg:h-[51px] md:rounded-[6px] lg:rounded-[10px] lg:pl-[14px] lg:pr-[16px] flex items-center justify-center md:justify-between gap-[10px] px-[5px]">
               <h2 className="lg:text-[16px] lg:leading-[24px] text-[10px] md:text-[12px] leading-[12px]">
                 Data Balance USSD Codes
@@ -409,61 +419,60 @@ const EtisalatDataBundle = () => {
           </div>
 
           {codes && (
-                <Modal>
-                (
-                <div
-                    className={`code ${
-                    toggleSideBar ? "xl:w-[65%] xl:ml-[17%] lg:ml-[20%] lg:w-[40%]" : "lg:w-[40%]"
-                    } w-[90%] xl:w-[80%] overflow-auto`}
+            <Modal>
+              (
+              <div
+                className={`code ${
+                  toggleSideBar
+                    ? "xl:w-[65%] xl:ml-[17%] lg:ml-[20%] lg:w-[40%]"
+                    : "lg:w-[40%]"
+                } w-[90%] xl:w-[80%] overflow-auto`}
+              >
+                <img
+                  onClick={() => setCodes(false)}
+                  className="absolute cursor-pointer right-2 w-[18px] h-[18px] my-[1%] md:w-[35px] md:h-[35px] lg:w-[25px] lg:h-[25px] xl:h-[35px] xl:w-[35px]"
+                  src="/Images/transferImages/close-circle.png"
+                  alt=""
+                />
+                <hr className="h-[6px] bg-[#04177f] border-none mt-[8%] md:mt-[6%] md:h-[10px]" />
+
+                <button
+                  className={`bg-[#04177f] my-[5%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[12px] font-extrabold h-[40px] text-white rounded-[6px] md:w-[50%] md:rounded-[8px] md:text-[16px] lg:text-[14px] xl:text-[20px] lg:w-[350px] lg:h-[38px] lg:my-[2%]`}
                 >
-                    <img
-                    onClick={()=> setCodes(false)}
-                    className="absolute cursor-pointer right-2 w-[18px] h-[18px] my-[1%] md:w-[35px] md:h-[35px] lg:w-[25px] lg:h-[25px] xl:h-[35px] xl:w-[35px]"
-                    src="/Images/transferImages/close-circle.png"
-                    alt=""
-                    />
-                    <hr className="h-[6px] bg-[#04177f] border-none mt-[8%] md:mt-[6%] md:h-[10px]" />
-
-                    <button
-                    className={`bg-[#04177f] my-[5%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[12px] font-extrabold h-[40px] text-white rounded-[6px] md:w-[50%] md:rounded-[8px] md:text-[16px] lg:text-[14px] xl:text-[20px] lg:w-[350px] lg:h-[38px] lg:my-[2%]`}
-                    >
-                    Data Balance USSD Codes
-                    </button>
-                    <h2 className="text-[12px] my-[5%] text-center md:my-[3%] md:text-[15px] lg:my-[2%] lg:text-[16px]">
-                    Data balance / share ussd codes.
-                    </h2>
-                    <h2 className="text-[12px] px-[2%] my-[5%] text-blue-600 text-center md:my-[3%] md:text-[15px] lg:my-[2%] lg:text-[16px]">
-                    Tap the network Dial button to check data balance:
-                    </h2>
-                    <div className='flex flex-col gap-1 mb-5'>
-                        
-                       
-                        <button
-                            className={`bg-[#FAF8F8] mt-[0%] mb-[2%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[14px] font-semibold h-[44px] shadow-md text-black rounded-[6px] md:w-[55%] md:mt-[0%] md:rounded-[8px] md:text-[16px] lg:text-[16px] lg:w-[410px] lg:h-[51px] lg:my-[2%] xl:mt-[0%] xl:mb-[1%]`}
-                            >
-                            9MOBILE Data Balance Code - *323#
-                        </button>
-                        <button
-                            className={`bg-[#FAF8F8] my-[2%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[14px] font-semibold h-[44px] shadow-md text-black rounded-[6px] md:w-[55%] md:rounded-[8px] md:text-[16px] lg:text-[16px] lg:w-[410px] lg:h-[51px] lg:my-[2%] xl:mt-[1%]`}
-                            >
-                            9MOBILE Data Share Code - *321#
-                        </button>
-                       
-                    </div>
-
-                    <button
-                    onClick={()=> setCodes(false)}
-                    className={`bg-[#04177f] my-[5%] mt-[40%] md:mt-[10%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[14px] font-extrabold h-[40px] text-white rounded-[6px] md:w-[25%] md:rounded-[8px] md:text-[16px] lg:text-[14px] lg:w-[163px] lg:h-[38px] lg:my-[2%] xl:mt-[9%]`}
-                    >
-                    Okay
-                    </button>
+                  Data Balance USSD Codes
+                </button>
+                <h2 className="text-[12px] my-[5%] text-center md:my-[3%] md:text-[15px] lg:my-[2%] lg:text-[16px]">
+                  Data balance / share ussd codes.
+                </h2>
+                <h2 className="text-[12px] px-[2%] my-[5%] text-blue-600 text-center md:my-[3%] md:text-[15px] lg:my-[2%] lg:text-[16px]">
+                  Tap the network Dial button to check data balance:
+                </h2>
+                <div className="flex flex-col gap-1 mb-5">
+                  <button
+                    className={`bg-[#FAF8F8] mt-[0%] mb-[2%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[14px] font-semibold h-[44px] shadow-md text-black rounded-[6px] md:w-[55%] md:mt-[0%] md:rounded-[8px] md:text-[16px] lg:text-[16px] lg:w-[410px] lg:h-[51px] lg:my-[2%] xl:mt-[0%] xl:mb-[1%]`}
+                  >
+                    9MOBILE Data Balance Code - *323#
+                  </button>
+                  <button
+                    className={`bg-[#FAF8F8] my-[2%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[14px] font-semibold h-[44px] shadow-md text-black rounded-[6px] md:w-[55%] md:rounded-[8px] md:text-[16px] lg:text-[16px] lg:w-[410px] lg:h-[51px] lg:my-[2%] xl:mt-[1%]`}
+                  >
+                    9MOBILE Data Share Code - *321#
+                  </button>
                 </div>
-                )
-                </Modal>
-            )}
+
+                <button
+                  onClick={() => setCodes(false)}
+                  className={`bg-[#04177f] my-[5%] mt-[40%] md:mt-[10%] w-[88%] flex justify-center items-center mx-auto cursor-pointer text-[14px] font-extrabold h-[40px] text-white rounded-[6px] md:w-[25%] md:rounded-[8px] md:text-[16px] lg:text-[14px] lg:w-[163px] lg:h-[38px] lg:my-[2%] xl:mt-[9%]`}
+                >
+                  Okay
+                </button>
+              </div>
+              )
+            </Modal>
+          )}
 
           {/* =========================PRODUCTS============================== */}
-          
+
           <div className="grid grid-cols-1 mt-[25px] md:grid-cols-2 gap-y-[20px] md:gap-x-[58.68px] lg:gap-x-[100px] md:gap-y-[15px] lg:gap-y-[25px] pb-[30px] lg:py-[30px] md:mt-[20px]">
             <div className="relative">
               <h2 className="lg:text-[18px] lg:leading-[24px] mb-1 text-[10px] md:text-[12px] font-[600] leading-[12px]">
@@ -629,7 +638,7 @@ const EtisalatDataBundle = () => {
                 </div>
               </div>
             </div>
-            
+
             <div>
               <div onClick={handleShowPayment}>
                 <h2 className="lg:text-[18px] mt-[5px] lg:leading-[24px] mb-2 text-[10px] md:text-[12px] font-[600] leading-[12px]">
@@ -647,7 +656,10 @@ const EtisalatDataBundle = () => {
                       </h2>
                     </li>
                   ) : (
-                    <h2 onClick={handleShowPayment} className="text-[10px] lg:text-[14px]">
+                    <h2
+                      onClick={handleShowPayment}
+                      className="text-[10px] lg:text-[14px]"
+                    >
                       Select Payment Method
                     </h2>
                   )}
@@ -673,11 +685,13 @@ const EtisalatDataBundle = () => {
                 </div>
               </div>
               {showPayment && (
-                <div className={`border md:rounded-[10px] lg:mt-2 rounded-[4px] absolute ${
-                  toggleSideBar
-                    ? "w-full md:w-[44.5%] lg:w-[45%] 2xl:w-[46%]"
-                    : "w-full md:w-[46%] 2xl:w-[46.5%]"
-                } bg-[#FFF] z-[100]`}>
+                <div
+                  className={`border md:rounded-[10px] lg:mt-2 rounded-[4px] absolute ${
+                    toggleSideBar
+                      ? "w-full md:w-[44.5%] lg:w-[45%] 2xl:w-[46%]"
+                      : "w-full md:w-[46%] 2xl:w-[46.5%]"
+                  } bg-[#FFF] z-[100]`}
+                >
                   {countryList.map((country) => (
                     <Payment
                       key={country.id}
@@ -1170,37 +1184,27 @@ const EtisalatDataBundle = () => {
               Proceed
             </button>
           </div>
-
-         
         </section>
-<<<<<<< HEAD
 
-=======
-        
->>>>>>> 4b4b7db70a45ed2c1bb82d7972ecd6bc7a63205c
-         {/* =======================FOOTER=================================== */}
-         <div
-            className={`${
-              isDarkMode ? "" : ""
-<<<<<<< HEAD
-            } flex gap-[15px] justify-center items-center mt-[100%] pb-[5%] md:pb-[2%] md:mt-[40%] lg:mt-[40%] lg:pb-0`}
-=======
-            } flex gap-[15px] justify-center items-center mt-[100%] pb-[25%] md:pb-[12%] md:mt-[40%] lg:mt-[40%] lg:pb-0`}
->>>>>>> 4b4b7db70a45ed2c1bb82d7972ecd6bc7a63205c
-          >
-            <div className="text-[10px] md:text-[12px] lg:text-[14px]">
-              You need help ?
-            </div>
-            <Link to="/ContactUs">
-              <div
-                className={`${
-                  isDarkMode ? "border" : "bg-[#04177f]"
-                } text-[10px] p-1 text-white rounded-[8px] lg:text-[18px]`}
-              >
-                Contact Us
-              </div>
-            </Link>
+        {/* =======================FOOTER=================================== */}
+        <div
+          className={`${
+            isDarkMode ? "" : ""
+          } flex gap-[15px] justify-center items-center mt-[100%] pb-[25%] md:pb-[12%] md:mt-[40%] lg:mt-[40%] lg:pb-0`}
+        >
+          <div className="text-[10px] md:text-[12px] lg:text-[14px]">
+            You need help ?
           </div>
+          <Link to="/ContactUs">
+            <div
+              className={`${
+                isDarkMode ? "border" : "bg-[#04177f]"
+              } text-[10px] p-1 text-white rounded-[8px] lg:text-[18px]`}
+            >
+              Contact Us
+            </div>
+          </Link>
+        </div>
       </div>
     </DashBoardLayout>
   );
