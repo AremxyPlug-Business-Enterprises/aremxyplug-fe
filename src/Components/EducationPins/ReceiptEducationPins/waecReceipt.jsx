eae53edfa7504bb2b47bd670970511f91512d222
balodim29 <balooladimeji29@gmail.com>
2023-11-04 18:26:04 +0100

    update

--- src/Components/EducationPins/ReceiptEducationPins/waecReceipt.jsx
+++ src/Components/EducationPins/ReceiptEducationPins/waecReceipt.jsx
@@ -15,12 +15,12 @@ export default function WaecReceipt() {
       educationPinEmail,
       paymentResult,
       setExamType,
-      waecAmount,
+      educationAmount,
       setQuantityResult,
       setEducationPinEmail,
       setEducationPinPhone,
       setPaymentResult,
-      setWaecAmount,
+      setEducationAmount,
       setWalletBalance
       }
        = useContext(ContextProvider);
@@ -39,7 +39,7 @@ export default function WaecReceipt() {
           setEducationPinPhone('');
           setEducationPinEmail('');
          setPaymentResult('');
-         setWaecAmount('₦');
+         setEducationAmount('₦');
          setWalletBalance('');
         }
       
@@ -178,7 +178,7 @@ export default function WaecReceipt() {
                 <div className="flex text-[10px] md:text-[14px] w-[90%]
                  mx-auto justify-between  lg:text-[16px] font-[500]">
                   <p className="text-[#0008]">Amount</p>
-                  <span>{waecAmount}</span>
+                  <span>{educationAmount}</span>
                 </div>            
               </div>
 
