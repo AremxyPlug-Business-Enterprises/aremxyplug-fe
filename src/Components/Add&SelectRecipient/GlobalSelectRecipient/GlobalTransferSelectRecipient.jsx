b0b4a02965391a511bc709d69e6cb4db9fd16385
balodim29 <balooladimeji29@gmail.com>
2023-12-09 18:34:14 +0100

    update

--- src/Components/Add&SelectRecipient/GlobalSelectRecipient/GlobalTransferSelectRecipient.jsx
+++ src/Components/Add&SelectRecipient/GlobalSelectRecipient/GlobalTransferSelectRecipient.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react';
 import '../../../App.css';
 import RecipientHero from '../RecipientHero';
 import { DashBoardLayout } from '../../Dashboard/Layout/DashBoardLayout';
 import ArrowRight from '../../EducationPins/imagesEducation/educationArrowRight.svg';
-import Nigerianflag from '../../EducationPins/imagesEducation/Nigeriaflag.svg';
 import ImageDrop from '../../EducationPins/imagesEducation/arrow-down.svg';
 import AddRecipientIcon from '../RecipientImages/AddRecipientIcon.svg';
 import SearchIcon from '../RecipientImages/search-status.svg';
 import NoRecordImage from '../RecipientImages/NoRecordImage.svg';
+import nigerianFlag from '../../EducationPins/imagesEducation/Nigeriaflag.svg';
+import americaFlag from '../../EducationPins/imagesEducation/Usa.svg';
+import britainFlag from '../../EducationPins/imagesEducation/Britain.svg';
+import austriaFlag from '../../EducationPins/imagesEducation/Austria.svg';
+import kenyaFlag from '../../EducationPins/imagesEducation/Kenya.svg';
+import imageCurrencyNotAvalaible from '../RecipientImages/CurrencyNotAvaliable.svg';
+import { Modal } from '../../Screens/Modal/Modal';
 import { Link } from 'react-router-dom';
-export default function GlobalTransferSelectRecipient() {
 
+export default function GlobalTransferSelectRecipient() {
+  const [selectRecipientCountry, setSelectRecipientCountry] = useState('');
+   const [selectRecipientCountryFlag,setSelectRecipientCountryFlag] = useState(false);
+  const [selectRecipientActive, setSelectRecipientActive] = useState(false);
+  const [CurrencyNotAvalaible, setCurrencyNotAvalaible]= useState(false);
+
+  const [recipientCountrySelection] = useState([
+    {method : 'Nigeria',  flag : nigerianFlag, id : 1},
+   {method : 'United states ',  flag : americaFlag, id : 2 },
+   {method : 'United Kingdom',flag : britainFlag, id : 3 },
+  //  {method :  'GBP Wallet', balance :'(0.00)', flag : euroFlag, id : 4 },
+   {method : 'Austria',  flag : austriaFlag, id : 4 },
+   {method : 'Kenya', flag : kenyaFlag, id: 5 }
+   ])
+//========== RECIPIENT DROPDOWN  ==============
+function RecipientDropDown(){
+  setSelectRecipientActive(!selectRecipientActive);
+document.querySelector('.SelectRecipientDrop').classList.toggle('DropIt');
+}
+
+// const ChosenRecipient  = () => {
+//   setSelectRecipientActive(false);
+//   document.querySelector('.SelectRecipientDrop').classList.remove('DropIt');
+//   if(recipientCountrySelection[0]){
+//    setSelectRecipientCountry("Nigeria");
+//    setSelectRecipientCountryFlag(nigerianFlag);
+//   }else {
+//     setCurrencyNotAvalaible(true);
+//   }
+// }
 
   return (
 
@@ -34,20 +69,53 @@ className='lg:w-[24px] lg:h-[24px] h-[10px] w-[10px] self-center'/>
     </h2>
     <div className='flex flex-col md:flex-row lg:gap-[100px] md:gap-[90px] gap-[25px] w-[100%] md:justify-between'>
       {/* CONTAINER FOR SELECT RECIPIENT */}
-   <div className='flex flex-col  md:w-[50%] w-[100%]'>
+   <div className='relative flex flex-col  md:w-[50%] w-[100%]'>
     {/* INPUTS TO SELECT RECIPIENT */}
-    <div className='flex justify-between py-[8.178px] pl-[3.672px] pr-[7px] lg:py-[14px] lg:pl-[16px] lg:pr-[10px]
+    <div  onClick={RecipientDropDown}
+    className='flex justify-between py-[8.178px] pl-[3.672px] pr-[7px] lg:py-[14px] lg:pl-[16px] lg:pr-[10px]
    md:border-[1px] border-[0.5px] border-[solid] border-[#7C7C7C] rounded-[3.671px] lg:rounded-[10px] bg-white'>
       <div className='flex lg:gap-[10px] gap-[5.868px]'>
-    <img src={Nigerianflag} alt="" 
+    <img src={selectRecipientCountryFlag} alt="" 
     className='lg:w-[24px] lg:h-[24px] h-[12px] w-[12px]'/>
    <h2 className='font-[600] text-[9px]  leading-[12px] lg:text-[16px] lg:leading-[20.8px]'>
-    Nigeria
+      {selectRecipientCountry}
     </h2>
    </div>
    <img src={ImageDrop}  alt=""
-   className='lg:w-[24px] lg:h-[24px] h-[12px] w-[12px]' />
+   className='SelectRecipientDrop lg:w-[24px] lg:h-[24px] h-[12px] w-[12px]' />
     </div>
+    {selectRecipientActive && (
+      <div className='absolute lg:top-[90px] md:top-[60px]  top-[50px] z-[2]  
+      flex flex-col w-[100%] lg:h-225px md:h-[210px]  '>
+      {(recipientCountrySelection.map(recipientCountry =>{
+        return (
+          <div  onClick={() => {
+            setSelectRecipientActive(false);
+            document.querySelector('.SelectRecipientDrop').classList.remove('DropIt')
+            if(recipientCountry.id === 1){
+              setSelectRecipientCountry("Nigeria");
+              setSelectRecipientCountryFlag(nigerianFlag);
+             }else {
+               setCurrencyNotAvalaible(true);
+             }
+          }}
+          className='flex gap-[10px] lg:py-[15px] py-[10px] pl-[10px]
+          cursor-pointer hover:bg-[#EDEAEA] items-center bg-white
+          shadow-[0px_3.30667px_8.26667px_0px_rgba(0,0,0,0.25)]'>
+
+          <img className='md:h-[29.27px]  h-[14.27px]' src={recipientCountry.flag} alt=""/>
+          <p className='text-[8px] leading-[10.4px] font-[500] text-[#7C7C7C]  
+         md:text-[13.227px] md:leading-[17.195px] 
+         lg:text-[16px] lg:leading-[20.8px] self-center cursor-pointer'
+          key={recipientCountry.id}> 
+          {recipientCountry.method}
+          </p>
+           </div>
+          
+        )
+      }) )}
+      </div>
+    ) }
      </div>
 
    {/* ADD RECIPIENTS */}
@@ -122,6 +190,41 @@ className='lg:w-[24px] lg:h-[24px] h-[10px] w-[10px] self-center'/>
                 Contact Us
               </Link>
             </div>
+            {CurrencyNotAvalaible && (
+              <Modal>
+              <div className='h-[100%] w-[100%] md:justify-center flex 
+              items-center  md:mx-[0px] mx-[19px]'>
+         <div className='flex flex-col  lg:w-[50%] h-[300px] w-[100%] lg:h-[550px] bg-white lg:rounded-[20px]
+          shadow-[0px_0px_6.933px_0px_rgba(0,0,0,0.25)] rounded-[8px]
+          lg:shadow-[0px_0px_20px_0px_rgba(0,0,0,0.25)] pt-[20px] pb-[20px] lg:pb-[80px] lg:pt-[80px]'>
+          <div className='flex flex-col justify-between  lg:w-[100%]   items-center h-[100%]'>
+  <div className='flex flex-col items-center lg:w-[80%] lg:gap-[20px] gap-[10px]'>
+   <h2 className='font-[600] text-[#04177F] text-center text-[10px] leading-[14px] lg:text-[16.647px] lg:leading-[24px]'>
+   This Currency is Currently Not Available.
+    </h2>
+    <img src={imageCurrencyNotAvalaible} alt=""
+    className='lg:w-[135px] lg:h-[135px] h-[100px] w-[100px]'/>
+    
+  </div>
+  <div className='flex md:gap-[30px] gap-[20px]'>
+  <button onClick={() => {
+    setCurrencyNotAvalaible(false);
+  }}
+className='bg-[#04177F] w-[80%] lg:py-[10px] py-[13px] lg:w-[163px]
+md:w-[150px] text-white text-center rounded-[4.41px]
+ lg:rounded-[12px] font-[600] lg:text-[16px] lg:leading-[24px]'>
+  Okay
+</button>
+<p className='font-[600] text-center text-[10px] leading-[14px] 
+lg:text-[16px] lg:leading-[24px] self-start'>
+Coming Soon...
+</p>
+</div>
+  </div>
+         </div>
+              </div>
+              </Modal>
+            )}
     </div>
 
   </DashBoardLayout>
