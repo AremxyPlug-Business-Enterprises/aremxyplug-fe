48f16315e105f76c382b2a6a5c4c16ea723be738
nomeo-310 <onome962@gmail.com>
2023-12-14 07:05:52 +0100

    changes made on payment page, sidebar, ngnvirtual account

--- src/Components/Screens/paymentPage/PaymentPage.jsx
+++ src/Components/Screens/paymentPage/PaymentPage.jsx
@@ -36,7 +36,7 @@ const cardPaymentData = [
     image: "./Images/paymentPage/internationalPayment.svg",
     title: "International Payments",
     content: "Send and receive money internationally without any hassle",
-    link: '/international-payment'
+    link: '/To-other-banks'
   }
 ];
 // FUNDING METHODS DATA ENDS HERE
@@ -78,7 +78,7 @@ const PaymentPage =() => {
       <div className="flex flex-col justify-between h-full">
         <div>
           {/* TOP BANNER STARTS HERE */}
-          <div className="w-full h-[90px] md:h-[112.29px] lg:h-[196px] rounded-[7px] md:rounded-[11.5px] backgroundImage flex items-center px-[16px] lg:px-[50px] justify-between lg:rounded-[20px]">
+          <div className="w-full h-[90px] md:h-[112.29px] lg:h-[196px] rounded-[7px] md:rounded-[11.5px] flex px-[16px] lg:px-[50px] justify-between items-center lg:rounded-[20px] backgroundImage">
             <div className='py-[13px] lg:py-[40px]'>
               <h2 className='text-[10px] md:text-[13.75px] font-bold mb-3 lg:text-[24px] lg:mb-4'>ENJOY QUICK AND SECURE PAYMENT PROCESSING.</h2>
               <h2 className='text-[7px] md:text-[11.46px] lg:text-[20px] lg:leading-[26px] mb-3'>To make sure your transactions go off without a hitch, using our user-friendly interface to receive money both locally and internationally.</h2>
