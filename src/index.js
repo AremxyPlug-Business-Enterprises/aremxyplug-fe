289b7312869f18b81a0da69b61af1109397897f4
Balogun Oladimeji <balooladimeji29@gmail.com>
2024-09-21 21:46:53 +0100

    tttt

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import { Context } from './Components/Context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+   
     <Context>
+      <Router>
     <App />
-    </Context>
     </Router>
+    </Context>
+    
   </React.StrictMode>
 );
 
