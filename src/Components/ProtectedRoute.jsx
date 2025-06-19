6259b6313b9166f35b331a0466a1da23f5941911
Balodim <balooladimeji29@gmail.com>
2025-06-19 10:13:35 +0000

    update

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,20 +1,68 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Navigate } from 'react-router-dom';
+import { HandleUserSession } from './ApiCollection.jsx/ApiBuck';
+import { RemoveLocalStorage } from './LocalStorage/LocalStorage';
 // import { RemoveLocalStorage } from './LocalStorage/LocalStorage';
 //import { ContextProvider } from './Context';
 export const ProtectedRoute = ({children}) => {
-    // window.onload(()=> {
+// The aim is to create three different situation when the user will
+// will be logged from the page
+// 1. The point in which the authToken is not gotten through cookies after a request from
+// an expitred token.The usage of user session expiration component must be 
+//used here.
+// 2. No user activity was detected within 20 minutes, a pop up for inactivity is created.
+// 3. The user enters the dashboard or any protected-route through an openend tab
+// for the opened tab a cookie present in the frontend is to check the time when it was created,
+// then does it substraction to know if expired or not.
+//4. The authorisedLogin, getToken and userStatus is not found in the local Storage.
 
-    // })
+//The immediate function below is to check for user activity
+//    const CheckUserActivity =(timeoutValue)=>{
+//   let TimeOut;
+//   const [numberCount, setNumberCount] = useState(6000)
+//     const events =["scroll", "click"];
+//    events.forEach((event)=> {
+//       console.log(event);
+//   window.addEventListener(event, ()=> {
+   
+//  setNumberCount(60)
+//    console.log(numberCount);
+//   TimeOut =  setTimeout(()=> {
+//      // console.log(window)
+//   return <Navigate to = "/Login" replace/>
+//    },numberCount)
+//    console.log(TimeOut)
+//    //return clearTimeout(TimeOut)
 
-
-
-
- const authToken = localStorage.getItem("authorisedLogin")
+//   })
+//  })
+// }
+    const authToken = localStorage.getItem("authorisedLogin")
  const getToken = localStorage.getItem("getToken");
  const UserStatus = localStorage.getItem("UserStatus");
  if((!authToken && !getToken) || !UserStatus  ){
     return <Navigate to ="/login" replace/>
  }
+
+ //Creating of auto Logout
+ const value = document.cookie;
+const checkSessionToken = value.split(",");
+console.log(checkSessionToken[1]);
+// console.log(value);
+window.addEventListener("visiblitychange", ()=> {
+ if(document.visibilityState === "visible" && UserStatus&& !checkSessionToken[1]?.startsWith("sessionToken")){
+ RemoveLocalStorage();
+ return <Navigate to ="/Login" replace/>
+}}
+)
+
+//   <HandleUserSession/>
+
+ 
+
+    // window.onload(()=> {
+
+    // })
+
  return children;
 }
